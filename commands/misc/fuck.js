8fe866b6ecbb5c67c13a9f9f8a3aed606bcaa610
StrafeStopper <strafestopper@gmail.com>
2024-07-18 01:26:53 -0600

    modified fuck command to add aditional text

--- commands/misc/fuck.js
+++ commands/misc/fuck.js
@@ -1,20 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	cooldown: 60,
+	cooldown: 10,
 	category: 'misc',
 	data: new SlashCommandBuilder()
 		.setName('fuck')
 		.setDescription('Fucks someone for you')
-        .addUserOption((option) => option.setName('user').setDescription('The user to fuck').setRequired(true)),
+        .addUserOption((option) => option.setName('user').setDescription('The user to fuck').setRequired(true))
+		.addStringOption( option => 
+			option.setName('text')
+			.setDescription('Add additional text to the message.')
+			.setRequired(false)),
 
 	async execute(interaction) {
-
+		
+		const text = interaction.options.getString('text');
 		if (interaction.options.getMember('user') == 551870229483880471)
 		{
 			await interaction.reply(`undefined` + ` fucked ${interaction.options.getMember('user')}`);
 		} else
-		await interaction.reply(`${interaction.member.user}` + ` fucked ${interaction.options.getMember('user')}`);
+		await interaction.reply(`${interaction.member.user}` + ` fucked ${interaction.options.getMember('user')}` + text);
 		
 	},
 };
