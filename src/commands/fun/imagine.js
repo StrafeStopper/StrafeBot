d297943b0f139775bff05ee41d5a19fac0a6c14d
StrafeStopper <strafestopper@gmail.com>
2024-07-26 08:19:53 -0600

    added ai image generation

---
+++ src/commands/fun/imagine.js
@@ -0,0 +1,82 @@
+const {
+  ApplicationCommandOptionType,
+  ButtonBuilder,
+  ButtonStyle,
+  ActionRowBuilder,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  EmbedBuilder,
+} = require("discord.js");
+const { REPLICATE_API_KEY } = require("../../config.json");
+const models = require("../../models");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("image-ai")
+    .setDescription("Generate an AI image using a prompt.")
+    .addStringOption((opt) =>
+      opt
+        .setName("prompt")
+        .setDescription("Enter your prompt")
+        .setRequired(true)
+    )
+    .addStringOption((opt) =>
+      opt
+        .setName("model")
+        .setDescription("The image model to use")
+        .setChoices(models)
+        .setRequired(true)
+    )
+    .toJSON(),
+  deleted: false,
+
+  userPermissions: [PermissionFlagsBits.SendMessages],
+  botPermissions: [PermissionFlagsBits.SendMessages],
+
+  run: async ( client, interaction ) => {
+    await interaction.deferReply();
+    try {
+      const { default: Replicate } = await import("replicate");
+
+      const replicate = new Replicate({
+        auth: REPLICATE_API_KEY,
+      });
+
+      const prompt = interaction.options.getString("prompt");
+      const model = interaction.options.getString("model") || models[0].value;
+
+      const output = await replicate.run(model, { input: { prompt } });
+
+      const row = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setLabel(`Download`)
+          .setStyle(ButtonStyle.Link)
+          .setURL(`${output[0]}`)
+          .setEmoji("1101133529607327764")
+      );
+
+      const resultEmbed = new EmbedBuilder()
+        .setTitle("Image Generated")
+        .addFields({ name: "Prompt", value: prompt })
+        .setImage(output[0])
+        .setColor("#44a3e3")
+        .setFooter({
+          text: `Requested by ${interaction.user.username}`,
+          iconURL: interaction.user.displayAvatarURL({ dynamic: true }),
+        });
+
+      await interaction.editReply({
+        embeds: [resultEmbed],
+        components: [row],
+      });
+    } catch (error) {
+      console.log(`An error occured:\n\n ${error}`);
+      /*const errEmbed = new EmbedBuilder()
+        .setTitle("An error occurred")
+        .setDescription("```" + error + "```")
+        .setColor(0xe32424);
+
+      return  interaction.editReply({ embeds: [errEmbed] });*/
+    }
+  },
+};
